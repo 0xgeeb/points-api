fbd67ab8403e9a3602f99cb96ef2d3bd8c28f677
0xgeeb <0xgeeb@gmail.com>
2025-02-19 18:12:16 -0500

    added rusd fixed apr endpoint

--- app.ts
+++ app.ts
@@ -1,11 +1,47 @@
 import dotenv from "dotenv"
 import express, { Request, Response } from "express"
 import { gql, request } from "graphql-request"
+import { createConfig, http } from "wagmi"
+import { formatEther, parseEther, type Chain } from "viem"
+import { getPublicClient, readContract } from "@wagmi/core"
+import vaultABI from './abis/WeethGoldivault.json'
+import quoterABI from "./abis/QuoterV2.json"
 
 dotenv.config()
 const app = express()
 const port = 3000
 app.use(express.json())
+
+const BerachainMainnet = {
+  id: 80094,
+  name: "Berachain",
+  nativeCurrency: {
+    name: "BERA",
+    symbol: "BERA",
+    decimals: 18
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.berachain.com/"],
+    },
+    public: {
+      http: ["https://rpc.berachain.com/"],
+    }
+  }
+} as const satisfies Chain
+
+export const config = createConfig({
+  chains: [BerachainMainnet],
+  ssr: true,
+  transports: {
+    [BerachainMainnet.id]: http()
+  }
+})
+
+const RUSD_ADDRESS = '0x09D4214C03D01F49544C0448DBE3A27f768F2b34'
+const RUSDOT_ADDRESS = '0x4A8B5283E053A8B118EaDc4981e8Ec8659995652'
+const RUSDVAULT_ADDRESS = '0x8f65453BF050233d3BD6a08A5Eb53C1fD73312EC'
+const QUOTER_ADDRESS = '0x644C8D6E501f7C994B74F5ceA96abe65d0BA662B'
 
 const RSETH_ISLANDHOLDERS_QUERY = gql`
   query {
@@ -124,6 +160,39 @@ app.get("/solvbtc", async (req: Request, res: Response) => {
     islandHolders: islandHoldersResponse?.solvbtcIslandHolders,
     ytHolders: ytResponse?.solvbtcYTHolders,
     islandBalances: islandBalancesResponse?.solvbtcIslandBalances
+  }
+  console.log(jsonResponse)
+  res.json(jsonResponse)
+})
+
+app.get("/dolomite", async (req: Request, res: Response) => {
+  console.log('dolomite api request received')
+  const endTimeResult: any = await readContract(config, {
+    address: RUSDVAULT_ADDRESS as `0x${string}`,
+    abi: vaultABI.abi,
+    functionName: "endTime",
+    args: []
+  })
+  let buyingOTQuoteResult: any = await readContract(config, {
+    address: QUOTER_ADDRESS as `0x${string}`,
+    abi: quoterABI.abi,
+    functionName: "quoteExactOutputSingle",
+    args: [[
+      RUSD_ADDRESS,
+      RUSDOT_ADDRESS,
+      parseEther(`1`),
+      500,
+      0
+    ]]
+  })
+  const endTime = parseFloat(endTimeResult)
+  const buyingOTPrice = parseFloat(formatEther(buyingOTQuoteResult[0] as unknown as bigint))
+  const timeDifference = endTime * 1000 - Date.now()
+  const fixedDaysDifference = timeDifference / (1000 * 60 * 60 * 24)
+  const daysTil = parseFloat(fixedDaysDifference.toFixed(2))
+  const fixedAprResponse = ((1 - buyingOTPrice) / 1) * 100 * (365 / daysTil)
+  const jsonResponse = {
+    rusdOTFixedApr: fixedAprResponse
   }
   console.log(jsonResponse)
   res.json(jsonResponse)
--- test.sh
+++ test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-curl -X GET "http://localhost:3000/rseth" -w "\n"
-sleep 1
-curl -X GET "http://localhost:3000/unibtc" -w "\n"
-sleep 1
-curl -X GET "http://localhost:3000/solvbtc" -w "\n"
-sleep 1
+# curl -X GET "http://localhost:3000/rseth" -w "\n"
+# sleep 1
+# curl -X GET "http://localhost:3000/unibtc" -w "\n"
+# sleep 1
+# curl -X GET "http://localhost:3000/solvbtc" -w "\n"
+# sleep 1
+
+curl -X GET "http://localhost:3000/dolomite" -w "\n"
